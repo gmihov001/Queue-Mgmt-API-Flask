7985d74aeeba6c401cfa4f381652ceabb64da6e7
George Mihov <gmihov001@gmail.com>
2021-07-09 16:49:22 +0000

    Updates

--- src/sms.py
+++ src/sms.py
@@ -4,8 +4,9 @@ import os
 def send(body='Some body', to=''):
     # Your Account Sid and Auth Token from twilio.com/console
     # DANGER! This is insecure. See http://twil.io/secure
-    account_sid = 'ACebcf1c431d0537bdb929713268a2e58f'
-    auth_token = 'b8c38652760d2384938d6c1041831ad0'
+    account_sid = os.getenv("sid")
+    auth_token = os.getenv("token")
+
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
