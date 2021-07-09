9bd50cfb67d3c8e558556e273cc11497059ce9cc
George Mihov <gmihov001@gmail.com>
2021-07-09 17:50:14 +0000

    Updates

--- src/main.py
+++ src/main.py
@@ -64,9 +64,16 @@ def add():
 def dequeue():
     call_person = queue.dequeue()
     phone = call_person['number']
-    send(body=f"{call_person['name']}, your table is ready!", to=phone)
-    response = {}
-    return jsonify(f"Texted {call_person['name']} at {call_person['number']}."), 200    
+    # send(body=f"{call_person['name']}, your table is ready!", to=phone)
+    
+    tmp_queue = queue.get_queue()
+    size = queue.get_size()
+    response = {
+        "message": f"Texted {call_person['name']} at {call_person['number']}.",
+        "queue_size": f"There are now {size} people in the queue",
+        "next": f"Next in line: {tmp_queue[size - 1]}" if size else "No next in line"
+    }
+    return jsonify(response), 200    
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
